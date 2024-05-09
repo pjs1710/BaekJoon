989df5a4af503e46ce83be217b1a119ae0a0467a
Park Jae Seong <timer973@naver.com>
2024-05-09 13:41:29 +0900

    test

--- src/main/java/org/example/baekjoon/Num_1157.java
+++ src/main/java/org/example/baekjoon/Num_1157.java
@@ -1,5 +1,10 @@
 package org.example.baekjoon;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Num_1157 {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int[] arr = new int[26];
 }
