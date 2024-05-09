67090c25d049a2b7fcf1da8767b2275540a59dfc
Park Jae Seong <timer973@naver.com>
2024-05-09 13:46:12 +0900

    test

--- src/main/java/org/example/baekjoon/Num_1157.java
+++ src/main/java/org/example/baekjoon/Num_1157.java
@@ -7,4 +7,6 @@ import java.io.InputStreamReader;
 public class Num_1157 {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int[] arr = new int[26];
+
+    // 자바 강의부터 듣기
 }
