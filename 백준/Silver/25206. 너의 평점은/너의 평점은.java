a53ceeeb51d40a7702a36b2d16a65a5f01624ea8
Park Jae Seong <152384393+pjs1710@users.noreply.github.com>
2024-10-02 11:57:12 +0900

    [Silver V] Title: 너의 평점은, Time: 168 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String[] subject = new String[20];
+        double[] score = new double[20];
+        String[] grade = new String[20];
+        double sum = 0;
+        double cnt = 0;
+        for (int i = 0; i < 20; i++) {
+            String[] split = sc.nextLine().split(" ");
+            subject[i] = split[0];
+            score[i] = Double.parseDouble(split[1]);
+            grade[i] = split[2];
+        }
+
+        for (int i = 0; i < 20; i++) {
+            if (!grade[i].equals("P")) {
+                if (grade[i].equals("A+")) {
+                    sum += score[i] * 4.5;
+                    cnt += score[i];
+                } else if (grade[i].equals("A0")) {
+                    sum += score[i] * 4.0;
+                    cnt += score[i];
+                } else if (grade[i].equals("B+")) {
+                    sum += score[i] * 3.5;
+                    cnt += score[i];
+                } else if (grade[i].equals("B0")) {
+                    sum += score[i] * 3.0;
+                    cnt += score[i];
+                } else if (grade[i].equals("C+")) {
+                    sum += score[i] * 2.5;
+                    cnt += score[i];
+                } else if (grade[i].equals("C0")) {
+                    sum += score[i] * 2.0;
+                    cnt += score[i];
+                } else if (grade[i].equals("D+")) {
+                    sum += score[i] * 1.5;
+                    cnt += score[i];
+                } else if (grade[i].equals("D0")) {
+                    sum += score[i] * 1.0;
+                    cnt += score[i];
+                } else {
+                    cnt += score[i];
+                }
+            }
+        }
+        double result = sum / cnt;
+        System.out.println(result);
+    }
+}
