85b3f038c4c607902bc0ea665c7f4c1be4ac6025
Park Jae Seong <timer973@naver.com>
2025-06-30 17:13:35 +0900

    25.06.30

--- src/main/java/org/example/baekjoon/study/No_10798.java
+++ src/main/java/org/example/baekjoon/study/No_10798.java
@@ -5,6 +5,26 @@ import java.util.*;
 
 /**
  * 세로 읽기
+ *
+ * 입력 :
+ * ABCDE
+ * abcde
+ * 01234
+ * FGHIJ
+ * fghij
+ *
+ * 출력 :
+ * Aa0FfBb1GgCc2HhDd3IiEe4Jj
+ *
+ * 입력 :
+ * AABCDD
+ * afzz
+ * 09121
+ * a8EWg6
+ * P5h3kx
+ *
+ * 출력 :
+ * Aa0aPAf985Bz1EhCz2W3D1gkD6x
  */
 
 public class No_10798 {
