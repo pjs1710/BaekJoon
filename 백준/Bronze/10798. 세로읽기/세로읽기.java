86ae0f2685eef7cf1d64d475445e94c71e3ddba1
Park Jae Seong <152384393+pjs1710@users.noreply.github.com>
2025-06-30 15:11:25 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 세로 읽기
+ */
+
+public class Main {
+
+    static String[][] input = new String[5][15];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 5; i++) {
+            String[] data = br.readLine().split("");
+            for (int j = 0; j < data.length; j++) {
+                input[i][j] = data[j];
+            }
+        }
+
+        printString();
+    }
+
+    public static void printString() {
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (input[j][i] != null) {
+                    System.out.print(input[j][i]);
+                }
+            }
+        }
+    }
+}
