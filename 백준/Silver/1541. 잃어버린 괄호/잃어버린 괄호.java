909ebb1addefb5d38f30faa40e35be0809a2e238
Park Jae Seong <152384393+pjs1710@users.noreply.github.com>
2025-07-11 20:02:56 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 108 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 백준 1541 : 잃어버린 괄호
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] data = br.readLine().split("-");
+        int result = sum(data[0]);
+        /**
+         * 만약에 data가 "-"가 없는 데이터라면 ex. 10+20+30
+         * 해당 전체 문자열이 data[0]이고 이를 sum 메서드 실행에 사용되는 파라미터로 받아짐
+         * 10, 20, 30이 splitNumbers로 들어가게 되고 이 값을 sum후 return
+         */
+        for (int i = 1; i < data.length; i++) {
+            result -= sum(data[i]);
+        }
+
+        System.out.println(result);
+    }
+
+    public static int sum(String numData) {
+        String[] splitNumbers = numData.split("\\+");
+        int total = 0;
+        for (String number : splitNumbers) {
+            total += Integer.parseInt(number);
+        }
+        return total;
+    }
+}
